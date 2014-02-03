5523d44d228029ec8a2c6cea45011e4d5ba9fd2f
usuario <usuario@10.0.2.15>
2014-02-03 17:55:13 +0100

    Versión 1

---
+++ PruebaGit/src/holamundo/HolaMundo.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package holamundo;
+
+/**
+ * @author HolaMundoPepito
+ * 
+ */
+public class HolaMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Holamundo");
+
+	}
+
+}
